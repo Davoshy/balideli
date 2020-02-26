a4f088435ad25a4832371d363795673896af53a5
Connor Reese <connor.reese13@gmail.com>
2020-02-26 14:01:39 +0800

    uncommented routes and controllers

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,16 +1,16 @@
-// const Restaurants = require("../models/restaurants");
-// const router = require("express").Router();
-//
-// router.get("/", (req, res) => {
-//   Restaurants.find().then(response => {
-//     res.send(response);
-//   });
-// });
-//
-// router.post("/", (req, res) => {
-//   Restaurants.create(req.body).then(response => {
-//     res.send(response);
-//   });
-// });
-//
-// module.exports = router;
+const Restaurants = require("../models/restaurants");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  Restaurants.find().then(response => {
+    res.send(response);
+  });
+});
+
+router.post("/", (req, res) => {
+  Restaurants.create(req.body).then(response => {
+    res.send(response);
+  });
+});
+
+module.exports = router;
