820ab85891c71d00c0104e497ac3f5758ebb8279
Davoshy <44115294+Davoshy@users.noreply.github.com>
2020-04-03 15:57:58 +0200

    minor change

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,46 +1,45 @@
-const Restaurants = require("../models/restaurants");
-const router = require("express").Router();
+const Restaurants = require('../models/restaurants')
+const router = require('express').Router()
 
-router.get("/", (req, res) => {
-  Restaurants.find({})
-    .populate("category")
-    .then(response => {
-      res.send(response);
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
+router.get('/', (req, res) => {
+	Restaurants.find({})
+		.populate('category')
+		.then(response => {
+			res.send(response)
+		})
+		.catch(err => {
+			res.send(err)
+		})
+})
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  Restaurants.create(req.body)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
+router.post('/', (req, res) => {
+	Restaurants.create(req.body)
+		.then(response => {
+			res.send(response.data)
+		})
+		.catch(err => {
+			res.send(err)
+		})
+})
 
-router.patch("/:id", (req, res) => {
-  Restaurants.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-    response => {
-      res.send(response);
-    }
-  );
-});
+router.patch('/:id', (req, res) => {
+	Restaurants.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+		response => {
+			res.send(response)
+		}
+	)
+})
 
 // Created Route for single Restaurant
-router.get("/:id", (req, res) => {
-  Restaurants.find({ _id: req.params.id })
-    .populate("category")
-    .then(restaurant => {
-      res.send(restaurant[0]);
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
+router.get('/:id', (req, res) => {
+	Restaurants.find({ _id: req.params.id })
+		.populate('category')
+		.then(restaurant => {
+			res.send(restaurant[0])
+		})
+		.catch(err => {
+			res.send(err)
+		})
+})
 
-module.exports = router;
+module.exports = router
